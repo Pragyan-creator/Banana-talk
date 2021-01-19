497b636376efc4402222effef051be9b387e4eea
Pragyan-creator <60147873+Pragyan-creator@users.noreply.github.com>
2021-01-20 01:17:10 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var translateButton = document.querySelector("#translate-button");
+
+translateButton.addEventListener("click", buttonClickHandler)
+
+// read input
+var translateInput = document.querySelector("#translate-input");
+
+// show output
+var translateOutput = document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+
+function buttonClickHandler(event) {
+    console.log("button clicked");
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+    
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
