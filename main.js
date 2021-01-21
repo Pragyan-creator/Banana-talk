9da5f53216a6f7d1f0fe18b218b433533b9ef45e
Pragyan-creator <60147873+Pragyan-creator@users.noreply.github.com>
2021-01-21 19:38:20 +0530

    refactor : Ajax

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
 var translateButton = document.querySelector("#translate-button");
 
-translateButton.addEventListener("click", buttonClickHandler)
-
-// read input
 var translateInput = document.querySelector("#translate-input");
 
-// show output
 var translateOutput = document.querySelector("#translate-output");
 
 var url = "https://api.funtranslations.com/translate/minion.json"
 
+translateButton.onclick=function(){
+    var input=translateInput.value
+    var finalUrl = `${url}?text=${input}`
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET',`${finalUrl}`)
+    xhr.send()
 
-function buttonClickHandler(event) {
-    console.log("button clicked");
-    var input = translateInput.value;
-    var finalURL = constructURL(input);
-    console.log(finalURL);
-    fetch(finalURL)
-        .then(response => response.json())
-        .then(json => {
-            translateOutput.innerText = json.contents.translated;
-        })
-        .catch(() => alert("some error occured"))
-    
-}
-
-function constructURL(inputText) {
-    var encodedURI = encodeURI(inputText);
-    return `${url}?text=${encodedURI}`;
+    xhr.onload=function(){
+        const data=JSON.parse(xhr.responseText)
+        console.log(data)
+        translateOutput.innerText=data.contents.translated
+    }
+    xhr.onerror=function(){
+        alert('Some error occured')
+    }
 }
